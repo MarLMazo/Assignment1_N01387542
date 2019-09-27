c91430d98dcdfe758f7d27ac50af5d516a2453a6
MarLMazo <marlmazo01@gmail.com>
2019-09-26 22:04:29 -0400

    My Third commit

--- Assignment1_N01387542_MarlMazo_5101B/SportsCarReview_N01387542_Assignment1B.aspx.cs
+++ Assignment1_N01387542_MarlMazo_5101B/SportsCarReview_N01387542_Assignment1B.aspx.cs
@@ -26,24 +26,25 @@ namespace Assignment1_N01387542_MarlMazo_5101B
                     string Reviewer_SportsCarColor = sportsCar_Color.Text.ToString();
                     string Reviewer_Review = reviewer_Review.Text.ToString();
                     int Reviewer_SportsCarSeat = Convert.ToInt32(sportsCar_Seat.Text);
-                    int Reviewer_SportsCarPrice = int.Parse(sportsCar_Price.Text,NumberStyles.Currency); //Numberstyles Resources in https://stackoverflow.com/questions/4094334/how-to-format-a-currency-string-to-integer by Khalos Year 2010
+                    int Reviewer_SportsCarPrice = int.Parse(sportsCar_Price.Text,NumberStyles.Currency);
+                    //Numberstyles Resources in https://stackoverflow.com/questions/4094334/how-to-format-a-currency-string-to-integer by Khalos Year 2010
+                    // Need to convert Car Price to a INT for example ( $64,000 to be converted to 64000) 
                     int Reviewer_OverAllStar = 0;
                     
 
                         if ((Reviewer_SportsCarPrice >= 1000000 && (Reviewer_SportsCarYear == "2019"|| Reviewer_SportsCarYear == "2018")) || 
-                           ((100000 < Reviewer_SportsCarPrice && Reviewer_SportsCarPrice < 1000000) && (Reviewer_SportsCarYear == "2019" || Reviewer_SportsCarYear == "2018")) ||
-                           (Reviewer_SportsCarPrice <= 100000 && (Reviewer_SportsCarYear == "2019" || Reviewer_SportsCarYear == "2018")))
+                           ((100000 < Reviewer_SportsCarPrice && Reviewer_SportsCarPrice < 1000000) && (Reviewer_SportsCarYear == "2019" || Reviewer_SportsCarYear == "2018")))
                             {
                                 Reviewer_OverAllStar = 5;  
                             }               
                         else if ((Reviewer_SportsCarPrice >= 1000000 && (Reviewer_SportsCarYear == "2017" || Reviewer_SportsCarYear == "2016")) ||
                                 ((100000 < Reviewer_SportsCarPrice && Reviewer_SportsCarPrice < 1000000) && (Reviewer_SportsCarYear == "2017" || Reviewer_SportsCarYear == "2016"))||
-                                (Reviewer_SportsCarPrice <= 100000 && (Reviewer_SportsCarYear == "2017" || Reviewer_SportsCarYear == "2016")) ||
                                 (Reviewer_SportsCarPrice <= 100000 && (Reviewer_SportsCarYear == "2019" || Reviewer_SportsCarYear == "2018")))
                             {
                                 Reviewer_OverAllStar = 4;
                             }
                         else if ((Reviewer_SportsCarPrice >= 1000000 && (Reviewer_SportsCarYear == "2015" || Reviewer_SportsCarYear == "2014_Older")) ||
+                                ((100000 < Reviewer_SportsCarPrice && Reviewer_SportsCarPrice < 1000000) && (Reviewer_SportsCarYear == "2015" || Reviewer_SportsCarYear == "2014_Older")) ||
                                 (Reviewer_SportsCarPrice <= 100000 && (Reviewer_SportsCarYear == "2017" || Reviewer_SportsCarYear == "2016")))
                             {
                                 Reviewer_OverAllStar = 3;
@@ -52,10 +53,7 @@ namespace Assignment1_N01387542_MarlMazo_5101B
                             {
                                 Reviewer_OverAllStar = 2;
                             }
-                        else if (Reviewer_SportsCarPrice <= 100000 && (Reviewer_SportsCarYear == "2015" || Reviewer_SportsCarYear == "2014_Older"))
-                            {
-                                Reviewer_OverAllStar = 1;
-                            }
+                       
 
                         box_Results.InnerHtml = "Thank you for giving us your Feedback on the SportsCar you bought<br>";
                         box_Results.InnerHtml+= "The System concludes that you got a " + Reviewer_SportsCarBrand + " with a model of " + Reviewer_SportsCarModel + " " + Reviewer_SportsCarYear+"<br>";
